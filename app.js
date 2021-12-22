ac2cc4d239fcb7e208427ff182bdcb3ff2de1a8d
Jayallstars <jayza0072555@gmail.com>
2021-12-22 13:51:20 +0700

    add date.js and change app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
-var items = ["ABC", "DEF", "CCC"];
+const items = ["ABC", "DEF", "CCC"];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,24 +14,27 @@ app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
 
-    const today = new Date();
+    let day = date.getDate();
 
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    var day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", { theDay: day, newListItem: items });
+    res.render("list", {listTitle: day, newListItem: items});
 });
 
 app.post("/", function(req, res) {
-    const todoList1 = req.body.todoList;
-    items.push(todoList1);
-    res.redirect("/");
-})
+
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItem: workItems});
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
---
+++ date.js
@@ -0,0 +1,23 @@
+//jshont esversion:6
+
+module.exports.getDate = function() {
+
+  let today = new Date();
+
+  let options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+  };
+  return today.toLocaleDateString("en-US", options);
+}
+
+module.exports.getDay = function() {
+
+  let today = new Date();
+
+  let options = {
+      weekday: "long",
+  };
+  return today.toLocaleDateString("en-US", options);
+}
